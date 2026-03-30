e5d63a6ca62c29fcd57f7f375d40d9412b7abbaa
obiajulu-dev <princeobiegenti@gmail.com>
2026-03-30 15:47:37 +0100

    added forgot password

--- frontend/components/onboarding3.tsx/index.tsx
+++ frontend/components/onboarding3.tsx/index.tsx
@@ -52,31 +52,40 @@ const Onboarding3 = () => {
   ];
 
   return (
-    <div className="max-w-5xl w-10/12 mx-auto grid place-content-center">
-      <p className=" text-3xl md:text-[40px] font-extrabold text-[#1A1F1D] mb-2 ">
-        What are your goals?
-      </p>
-      <p className=" text-base md:text-lg font-normal text-[#404940]">
-        Select the primary outcomes you hope to achieve with TalentFlow.
-      </p>
-      <div className="mt-8 grid gap-3">
-        {goals.map((goal) => {
-          const isSelected = openItems.includes(goal.id);
-          return (
-            <div
-              key={goal.title}
-              onClick={() => toggleItem(goal.id)}
-              className={` p-2 sm:p-4 rounded-md cursor-pointer ${openItems.includes(goal.id) ? "bg-primary-500" : ""}`}
-            >
-              <Onboarding3Data
-                icon={goal.icon}
-                title={goal.title}
-                description={goal.description}
-                isSelected={isSelected}
-              />
-            </div>
-          );
-        })}
+    <div className="min-h-screen flex flex-col justify-between bg-[#f6f8f7]">
+      {/* Top Step Indicator */}
+      <div className="text-center w-full text-sm tracking-widest text-gray-400 mb-6 mt-4">
+        STEP 3 OF 4
+      </div>
+      <div className="flex flex-1 items-center bg-white justify-center px-4">
+        <div className="rounded-2xl w-full max-w-xl p-8">
+          <p className="text-2xl md:text-[30px] font-extrabold text-[#1A1F1D] mb-2">
+            What are your goals?
+          </p>
+          <p className="text-base md:text-lg font-normal text-[#404940]">
+            Select the primary outcomes you hope to achieve with TalentFlow.
+          </p>
+          <div className="mt-8 grid gap-3">
+            {goals.map((goal) => {
+              const isSelected = openItems.includes(goal.id);
+              return (
+                <div
+                  key={goal.title}
+                  onClick={() => toggleItem(goal.id)}
+                  className={`p-2 sm:p-4 rounded-xl text-black cursor-pointer ${openItems.includes(goal.id) ? "bg-primary-500 text-white" : ""
+                    }`}
+                >
+                  <Onboarding3Data
+                    icon={goal.icon}
+                    title={goal.title}
+                    description={goal.description}
+                    isSelected={isSelected}
+                  />
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </div>
     </div>
   );
@@ -96,9 +105,10 @@ const Onboarding3Data = ({
   isSelected: boolean;
 }) => {
   return (
-    <div className="flex flex-col sm:flex-row items-start sm:items-center gap-4">
+    <div className="flex flex-col sm:flex-row items-start gap-4 sm:items-center ">
       <div
-        className={`h-11 w-11 flex items-center justify-center rounded-md bg-[#E7E8E6] ${isSelected ? "bg-white/20" : ""} `}
+        className={`h-11 w-11 flex items-center justify-center rounded-md bg-[#E7E8E6] ${isSelected ? "bg-white/20" : ""
+          }`}
       >
         {Icon && (
           <Icon
@@ -106,34 +116,39 @@ const Onboarding3Data = ({
           />
         )}
       </div>
-      <div>
-        <p className={`text-lg font-bold`}>{title}</p>
-        <p className={`text-sm font-normal `}>{description}</p>
-      </div>
-      <input
-        type="checkbox"
-        checked={isSelected}
-        className={`
+
+      <div className=' flex flex-col sm:flex-row  sm:items-center justify-between w-full '>
+        <div>
+          <p className={`text-lg font-bold`}>{title}</p>
+          <p className={`text-sm font-normal`}>{description}</p>
+        </div>
+
+        <input
+          type="checkbox"
+          checked={isSelected}
+          readOnly
+          className={`
           ${isSelected ? "opacity-100 scale-100" : "opacity-0 scale-0"}
-    w-6 h-6
-    rounded-full
-    bg-white
-    appearance-none
-    cursor-pointer
-    relative
-    after:content-["✓"]
-    after:text-black
-    after:absolute
-    after:top-1/2
-    after:left-1/2
-    after:-translate-x-1/2
-    after:-translate-y-1/2
-    after:text-sm
-    after:font-bold
-    after:hidden
-    checked:after:block
-  `}
-      />
+          w-6 h-6
+          rounded-full
+          bg-white
+          appearance-none
+          cursor-pointer
+          relative
+          after:content-["✓"]
+          after:text-black
+          after:absolute
+          after:top-1/2
+          after:left-1/2
+          after:-translate-x-1/2
+          after:-translate-y-1/2
+          after:text-sm
+          after:font-bold
+          after:hidden
+          checked:after:block
+        `}
+        />
+      </div>
     </div>
   );
 };
